eb4bfda8624f5723169051e84337ead6dea31302
likekobe <476956004@qq.com>
2016-07-19 17:39:26 +0800

    table.sql

--- database/table.sql
+++ database/table.sql
@@ -1,5 +1,5 @@
 
-
+prompt Creating ECMS.SYS_FUNCTION...
 	create table ECMS.SYS_FUNCTION
 	(
 	  ID         CHAR(32) not null,
@@ -13,5 +13,60 @@
 	;
 	alter table ECMS.SYS_FUNCTION
 	  add constraint PK_SYS_FUNCTION primary key (ID);
+
+prompt Creating ECMS.US_USER_INFOR...
+	create table ECMS.US_USER_INFOR
+	(
+	  ID          CHAR(32) not null,
+	  NAME        VARCHAR2(60) not null,
+	  PASSWORD    VARCHAR2(20) not null,
+	  CREATE_DATE VARCHAR2(19) not null,
+	  MODIFY_DATE VARCHAR2(19),
+	  USERNAME    VARCHAR2(60),
+	  TELEPHONE   VARCHAR2(20),
+	  EMAIL       VARCHAR2(30),
+	  IS_DISABLE  CHAR(1) not null
+	)
+	;
+	alter table ECMS.US_USER_INFOR
+	  add constraint PK_US_USER_INFOR primary key (ID);
+	  
+	  
+prompt Creating ECMS.US_ROLE...
+	create table ECMS.US_ROLE
+	(
+	  ID     CHAR(32) not null,
+	  NAME   VARCHAR2(60) not null,
+	  REMARK VARCHAR2(100 char)
+	)
+	;
+	alter table ECMS.US_ROLE
+	  add constraint PK_US_ROLE primary key (ID);
+	  
+	  
+prompt Creating ECMS.US_MID_USER_ROLE...
+	create table ECMS.US_MID_USER_ROLE
+	(
+	  ID      CHAR(32) default SYS_guid() not null,
+	  USER_ID CHAR(32) not null,
+	  ROLE_ID CHAR(32) not null
+	)
+	;
+	alter table ECMS.US_MID_USER_ROLE
+	  add constraint PK_US_MID_USER_ROLE primary key (ID);
+
+prompt Creating ECMS.US_MID_FUNCTION_ROLE...
+	create table ECMS.US_MID_FUNCTION_ROLE
+	(
+	  ID          CHAR(32) default SYS_guid() not null,
+	  ROLE_ID     CHAR(32) not null,
+	  FUNCTION_ID CHAR(32) not null
+	)
+	;
+	alter table ECMS.US_MID_FUNCTION_ROLE
+	  add constraint PK_US_MID_FUNCTION_ROLE primary key (ID);
+
+
+
   
 	commit;
